42899a7267924cf5d3517585f8f1002fcbe97a3c
LuchoVidalSMN <lucianovidal78@gmail.com>
2026-09-17 17:05:16 -0300

    Agregamos icono de disdrometro en el titulo

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ with col_icon:
 
 with col_title:
     # Ajustamos un poco el margen superior usando markdown/HTML para alinearlo perfectamente con el centro del ícono
-    st.markdown('<h1 style="margin-top: -15px;">Disdrómetro SMN-Dorrego | Dashboard Interactivo (EXPERIMENTAL)</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="margin-top: -12px;">Disdrómetro SMN | Dashboard Interactivo</h1>', unsafe_allow_html=True)
 
 # --- ENCABEZADO DEL INSTRUMENTO ---
 st.markdown("---")
